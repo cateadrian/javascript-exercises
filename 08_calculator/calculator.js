06c0d8872c429727e9f1507fc1401659b73639d0
Adrian Cate <77905982+cateadrian@users.noreply.github.com>
2024-06-21 14:16:30 -0600

    08_calculator Done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, item) => {
+    return total + item;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return array.reduce((total, item) => {
+    return total * item;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+// Iterative version
+//	let total = 1;
+//  for (a; a > 0; a --){
+//    total *= a;
+//  }
+//  return total;
+
+// Recursive version
+  if(a == 0){
+    return 1;
+  } else {
+    return a * factorial(a-1);
+  };
 };
 
 // Do not edit below this line
